2ca1aae9a4ccfcec2054cc31f5e21b7ccd9dfd9f
Geoff S <matthew.kuchurean@flemingcollege.ca>
2023-03-29 14:43:04 -0400

    Update lab9API-Viewer.py

--- lab9API-Viewer.py
+++ lab9API-Viewer.py
@@ -1,8 +1,18 @@
 from tkinter import * 
 from tkinter import ttk 
-
+#from 
 #Create a window 
 # Create the window
+
+
+def handle_getinfo():
+    #poke_name =ent_name.get()
+    #poke information from pokemon API 
+    #poke_info = get_pokemon_info()
+    return
+
+# lbl_height_value ['text] = f"{'poke_info['height]}dm"
+#bar_hp['value']
 root = Tk()
 root.title("Pokemon Information Viewer ")
 # TODO: Additional window configuration
@@ -25,12 +35,34 @@ lbl_name.grid(row=0, column=0 , padx= (10,20) ,pady= (10,20))
 ent_name = ttk.Entry(frm_top)
 ent_name.grid(row=0, column=1, padx=(10,20), pady=(10,20))
 
-def handle_getinfo():
-    return
 
 btn_hello = ttk.Button(frm_top, text='Get Info', command=handle_getinfo)
 btn_hello.grid(row=0, column= 2, padx=15, pady=20)
 
+# Info frame in the populate widget 
+lbl_height = ttk.Label(frm_btm_left, text='Height:')
+lbl_height.grid(row=0, column=0, padx= 10 , pady= 20)
 
-root.mainloop()
+lbl_hp= ttk.Label(frm_btm_right, text='HP:')
+lbl_hp.grid(row=0, column=0, padx= 10 , pady= 20) 
+
+#Widget for stats frame 
+#bar_hp = ttk.Progressbar(frm_btm_right, orient=HORIZONTAL, length=200, maximum =255)
+#bar_hp.grid (row=1, column=1)
+#bar_hp['value'] = 123
+
+#lbl_attack= ttk.Label(frm_btm_right, text='Attack:')
+#lbl_attack.grid(row=1, column=1, padx= (10,20) , pady= (10,20))
+#bar_attack = ttk.Progressbar(frm_btm_right, orient=HORIZONTAL, length=200, maximum =255)
+#bar_attack.grid (row=1, column=0)
+
+#lbl_defence= ttk.Label(frm_btm_right, text='Defence:')
+#lbl_defence.grid(row=0, column=1, padx= (10,20) , pady= (10,20))
+#bar_defence = ttk.Progressbar(frm_btm_right, orient=HORIZONTAL, length=200, maximum =255)
+#bar_defence.grid (row=1, column=0)
+
+
+
+# TODO: Add weight and height 
+#root.mainloop()
 
